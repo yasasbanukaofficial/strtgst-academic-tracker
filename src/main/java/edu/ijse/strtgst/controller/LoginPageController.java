81bc461321d8707fa16b09bd9b4091a799bfe537
unknown <yasasbanukaofficial@gmail.com>
2025-04-09 01:00:45 +0530

    feat: implemented navigation from login page to sign up page via sign up btn

--- src/main/java/edu/ijse/strtgst/controller/LoginPageController.java
+++ src/main/java/edu/ijse/strtgst/controller/LoginPageController.java
@@ -1,4 +1,25 @@
 package edu.ijse.strtgst.controller;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Alert;
+import javafx.scene.layout.AnchorPane;
+
 public class LoginPageController {
+    public AnchorPane loginAnc;
+
+    public void visitSignUpPage(ActionEvent actionEvent) {
+        navigateTo("SignUpPage.fxml");
+    }
+
+    public void navigateTo(String path){
+        try{
+            loginAnc.getChildren().clear();
+            AnchorPane anchorPane = FXMLLoader.load(getClass().getResource("/view/" + path));
+            loginAnc.getChildren().add(anchorPane);
+        } catch (Exception e) {
+            new Alert(Alert.AlertType.ERROR, "Cant Identify the url path");
+            e.printStackTrace();
+        }
+    }
 }
