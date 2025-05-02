008fe7d2483aa0c8050ea4e0cfe148a59628ead8
unknown <yasasbanukaofficial@gmail.com>
2025-05-02 23:28:19 +0530

    feat: added CrudUtil, SignUpModel and updated DBConnection

--- src/main/java/edu/ijse/strtgst/db/DBConnection.java
+++ src/main/java/edu/ijse/strtgst/db/DBConnection.java
@@ -12,16 +12,12 @@ public class DBConnection {
     private final String user = "root";
     private final String password = "Mysql@123";
 
-    private DBConnection() throws ClassNotFoundException, SQLException {
-        Class.forName("com.mysql.cj.jdbc.Driver");
+    private DBConnection() throws SQLException {
         connection = DriverManager.getConnection(url, user, password);
     }
 
-    public static DBConnection getInstance() throws ClassNotFoundException, SQLException {
-        if (dbConnection == null){
-            dbConnection = new DBConnection();
-        }
-        return dbConnection;
+    public static DBConnection getInstance() throws SQLException {
+        return dbConnection == null ? new DBConnection() : dbConnection;
     }
 
     public Connection getConnection() {
---
+++ src/main/java/edu/ijse/strtgst/util/CrudUtil.java
@@ -0,0 +1,30 @@
+package edu.ijse.strtgst.util;
+
+import edu.ijse.strtgst.db.DBConnection;
+import javafx.scene.control.Alert;
+import lombok.AllArgsConstructor;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class CrudUtil {
+    public static <T> T execute(String sql, Object... obj) throws SQLException{
+        Connection connection = DBConnection.getInstance().getConnection();
+        PreparedStatement pst = connection.prepareStatement(sql);
+
+        for (int i = 0; i < obj.length; i++) {
+            pst.setObject(i + 1, obj[i]);
+        }
+
+        if (sql.startsWith("select") || sql.startsWith("SELECT")){
+            ResultSet rst = pst.executeQuery();
+            return (T) rst;
+        } else {
+            int i = pst.executeUpdate();
+            boolean isSuccess = i > 0;
+            return (T) (Boolean) isSuccess;
+        }
+    }
+}
