a70b05ef86d3d873f432d12262d9d81a6387845a
unknown <yasasbanukaofficial@gmail.com>
2025-05-03 14:01:56 +0530

    feat(signup): implemented regex validation for user input in SignUp

--- src/main/java/edu/ijse/strtgst/controller/SignUpPageController.java
+++ src/main/java/edu/ijse/strtgst/controller/SignUpPageController.java
@@ -18,6 +18,11 @@ public class SignUpPageController{
 
     private final StudentModel studentModel = new StudentModel();
 
+    private final String usernamePattern = "^[a-zA-Z0-9_-]{3,}$";
+    private final String emailPattern = "^((?!\\.)[\\w\\-_.]*[^.])(@\\w+)(\\.\\w+(\\.\\w+)?[^.\\W])$";
+    private final String passwordPattern = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=.\\-_*])[a-zA-Z0-9@#$%^&+=.\\-_]{3,}$";
+
+
     public void visitLoginPage(ActionEvent actionEvent) {
         navigateTo("LoginPage.fxml");
     }
@@ -38,19 +43,60 @@ public class SignUpPageController{
         String username = txtUsername.getText();
         String email = txtEmail.getText();
         String password = txtPassword.getText();
-
         StudentDto studentDto = new StudentDto(username, email, password);
-        try{
-            boolean isSaved = studentModel.addStudent(studentDto);
-            if(isSaved){
-                new Alert(Alert.AlertType.INFORMATION, "Successfully Saved user").show();
-                navigateTo("MainPage.fxml");
-            } else {
+
+        if (validateInputs(username, email, password)) {
+            try {
+                boolean isSaved = studentModel.addStudent(studentDto);
+                if (isSaved) {
+                    new Alert(Alert.AlertType.INFORMATION, "Successfully Saved user").show();
+                    navigateTo("MainPage.fxml");
+                } else {
+                    new Alert(Alert.AlertType.ERROR, "Failed when saving user").show();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
                 new Alert(Alert.AlertType.ERROR, "Failed when saving user").show();
             }
-        } catch (Exception e){
-            e.printStackTrace();
-            new Alert(Alert.AlertType.ERROR, "Failed when saving user").show();
         }
+    }
+
+    private boolean validateInputs (String username, String email, String password) {
+        StringBuilder errorMessage = new StringBuilder();
+        boolean isValid = true;
+        boolean isValidUserName = username.matches(usernamePattern);
+        boolean isValidEmail = email.matches(emailPattern);
+        boolean isValidPassword = password.matches(passwordPattern);
+
+        if (!isValidUserName) {
+            txtUsername.setStyle("-fx-border-color: #ce0101; -fx-border-radius: 10px; -fx-border-width: 2px; -fx-background-radius: 10px");
+            errorMessage.append("• Username must be at least 3 characters long and contain only letters, digits, underscores or hyphens.\n");
+            isValid = false;
+        }
+        if (!isValidEmail) {
+            txtEmail.setStyle("-fx-border-color: #ce0101; -fx-border-radius: 10px; -fx-border-width: 2px; -fx-background-radius: 10px");
+            errorMessage.append("• Email must be valid (e.g., name@example.com).\n");
+            isValid = false;
+        }
+        if (!isValidPassword) {
+            txtPassword.setStyle("-fx-border-color: #ce0101; -fx-border-radius: 10px; -fx-border-width: 2px; -fx-background-radius: 10px");
+            errorMessage.append("• Password must be more than 3 characters and should include one uppercase, lowercase, number, and special character.\n");
+            isValid = false;
+        }
+
+        if (isValidUserName) {
+            txtUsername.setStyle("-fx-border-color: #000000; -fx-border-radius: 10px; -fx-border-width: 2px; -fx-background-radius: 10px");
+        }
+        if (isValidEmail){
+            txtEmail.setStyle("-fx-border-color: #000000; -fx-border-radius: 10px; -fx-border-width: 2px; -fx-background-radius: 10px");
+        }
+        if (isValidPassword){
+            txtPassword.setStyle("-fx-border-color: #000000; -fx-border-radius: 10px; -fx-border-width: 2px; -fx-background-radius: 10px");
+        }
+
+        if (!isValid){
+            new Alert(Alert.AlertType.ERROR, "Please fix the following: \n\n" + errorMessage).show();
+        }
+        return isValid;
     }
 }
