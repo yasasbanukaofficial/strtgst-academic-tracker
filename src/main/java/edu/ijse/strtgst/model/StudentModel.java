815885926c9be214706bf5e115d80e8edb4b5b40
unknown <yasasbanukaofficial@gmail.com>
2025-05-04 16:38:39 +0530

    feat(student): add Id gen logic to auto-increment student id

--- src/main/java/edu/ijse/strtgst/model/StudentModel.java
+++ src/main/java/edu/ijse/strtgst/model/StudentModel.java
@@ -3,9 +3,25 @@ package edu.ijse.strtgst.model;
 import edu.ijse.strtgst.dto.StudentDto;
 import edu.ijse.strtgst.util.CrudUtil;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class StudentModel {
+    public String getNextID() throws SQLException{
+        ResultSet rst = CrudUtil.execute("SELECT stud_id FROM Student ORDER BY stud_id DESC LIMIT 1");
+        char firstCharacter = 'S';
+
+        if (rst.next()){
+            String lastId = rst.getString(1);
+            String lastIdNumString = lastId.substring(1);
+            int lastIdNumber = Integer.parseInt(lastIdNumString);
+            int nextIdNumber = lastIdNumber + 1;
+            String nextIdString = String.format(firstCharacter + "%03d" + nextIdNumber);
+            return nextIdString;
+        }
+        return firstCharacter + "001";
+    }
+
     public boolean addStudent(StudentDto studentDto) throws SQLException{
         return CrudUtil.execute(
                 "INSERT INTO Student (username, email, password) VALUES (?,?,?)",
